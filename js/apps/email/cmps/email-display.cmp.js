6be5131c9c916fa0832736faa1712da24f83d707
Simon Ifergan <simonifergan239@gmail.com>
2019-02-27 11:43:29 +0200

    changes

--- js/apps/email/cmps/email-display.cmp.js
+++ js/apps/email/cmps/email-display.cmp.js
@@ -1,11 +1,12 @@
 import emailService from '../services/email.service.js';
+import utilService from '../../../services/util.service.js';
 
 export default {
     template: `
         <section v-if="email" class="email-display">
             <header class="email-display-header">
-                <h1>{{email.subject}}</h1>
-                <div>{{email.sentAt}}</div>
+                <h1 class="subject">{{email.subject}}</h1>
+                <div class="sent-date">{{formattedDate}}</div>
             </header>
             <h2><span>From: </span>{{email.sender}}</h2>
             <p>{{email.body}}</p>
@@ -15,6 +16,11 @@ export default {
         return {
             email: null,
         };
+    },
+    computed: {
+        formattedDate() {
+            return utilService.formatDate(this.email.sentAt);
+        }
     },
     created() {
         let emailId = this.$route.params.emailId;
