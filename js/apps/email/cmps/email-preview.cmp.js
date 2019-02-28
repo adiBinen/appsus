e878a0a0d6c0ec97ad0320c9511d35d32e581754
Adi Binenbaum <adi.binen@gmail.com>
2019-02-28 12:54:31 +0200

    d

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -5,18 +5,22 @@ export default {
     props: ['email'],
     template: ` 
         <router-link tag="li" :to="email.id" exact class="email-preview flex" :class="setUnread">
-           <div class="sender">
+            <label class="checkbox-container" @click.stop>
+                <input type="checkbox">
+                <span class="checkmark"></span>
+            </label>
+            <div class="sender">
                 {{email.sender}}
-           </div>
-           <div class="subject">
+            </div>
+            <div class="subject">
                 {{email.subject}}
-           </div>
-           <div class="body">
+            </div>
+            <div class="body">
                 {{email.body}}
-           </div>
-           <div class="date">
+            </div>
+            <div class="date">
                 {{formattedDate}}
-           </div>
+            </div>
         </router-link>
     `,
     data() {
