4b723321bead03d2cbc702ecff13daa83b175d26
Simon Ifergan <simonifergan239@gmail.com>
2019-02-28 12:54:14 +0200

    email note

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -1,4 +1,3 @@
-import { eventBus, UNREAD_EMAILS } from '../../../event-bus.js';
 
 import emailService from '../services/email.service.js';
 import emailPreview from '../cmps/email-preview.cmp.js';
@@ -19,14 +18,10 @@ export default {
     data() {
         return {
             emails: null,
-            unreadEmails: null,
         }
     },
     created() {
         emailService.query()
             .then(emails => this.emails = emails);
-
-        emailService.getUnreadEmails()
-            .then(unreadNum => eventBus.$emit(UNREAD_EMAILS, unreadNum));
     }
 }
