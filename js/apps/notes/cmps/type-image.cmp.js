5e2a51a415753fcca337a42bf81a8548f79559a7
Simon <simonifergan239@gmail.com>
2019-03-01 10:07:42 +0200

    type done

--- js/apps/notes/cmps/type-image.cmp.js
+++ js/apps/notes/cmps/type-image.cmp.js
@@ -3,8 +3,24 @@ export default {
     props: ['data', 'isEditable'],
     template: `
         <div class="type-image">
-            <input v-model="data" v-show="isEditable">
+            <input v-model="dataCopy" v-show="isEditable">
             <img :src="data" />
         </div>
     `,
+    data() {
+        return {
+            isEditing: false,
+            dataCopy: this.data,
+        };
+    },
+    methods: {
+
+    },
+    watch: {
+        dataCopy: {
+            handler(val) {
+                this.$emit('update-data', val);
+            }
+        }
+    }
 }
